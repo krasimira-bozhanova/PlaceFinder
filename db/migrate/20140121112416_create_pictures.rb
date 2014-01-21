f1838fbd9cf9710c9f6c602579c90eb000093c75
krasimira-bozhanova <krasimira.bozhanova@gmail.com>
2014-01-21 11:51:25 +0200

    Add the created with rake migration files

---
+++ db/migrate/20140121112416_create_pictures.rb
@@ -0,0 +1,12 @@
+class CreatePictures < ActiveRecord::Migration
+  def up
+    create_table :pictures do |t|
+      t.integer :place_id,   :null => false
+      t.binary :picture
+    end
+  end
+
+  def down
+    drop_table :pictures
+  end
+end
